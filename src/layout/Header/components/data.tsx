f943abe8559c1520051a02db3056c6af5cb394e5
lethihongnhan2004 <nl422864@gmail.com>
2025-02-08 17:05:42 +0700

    fix : fix header in branch input

--- src/layout/Header/components/data.tsx
+++ src/layout/Header/components/data.tsx
@@ -2,6 +2,7 @@ import IconWishlist from "@/layout/assets/icons/IconAccount";
 import IconAccount from "@/layout/assets/icons/IconAccount";
 import IconCart from "@/layout/assets/icons/IconCart";
 import IconCompare from "@/layout/assets/icons/IconCompare";
+import IconHumburger from "@/layout/assets/icons/IconHamburger";
 import IconSearch from "@/layout/assets/icons/IconSearch";
 
 export const DATA = {
@@ -22,3 +23,4 @@ export const SEARCH_DATA = {
   categories: ["All Categories", "Xe dap 1", "Xe dap 2"],
   icons: [{ key: "search", icon: IconSearch }],
 };
+export const HEADER_MOBILE_DATA = [{ icon: IconHumburger }];
