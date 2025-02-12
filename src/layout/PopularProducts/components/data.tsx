9839b5205d298e350c35a62aea91a0c44b09f5b9
lethihongnhan2004 <nl422864@gmail.com>
2025-02-12 21:50:49 +0700

    update: css boxListProducts

--- src/layout/PopularProducts/components/data.tsx
+++ src/layout/PopularProducts/components/data.tsx
@@ -10,5 +10,47 @@ const productData = [
     weight: "500 gram",
     brand: "Hodo Foods",
   },
+  {
+    id: 2,
+    name: "All Natural Italian-Style Chicken Meatballs",
+    imageUrl: <IconNgo />,
+    rating: 5,
+    originalPrice: 2.8,
+    discountedPrice: 2.51,
+    weight: "500 gram",
+    brand: "Hodo Foods",
+  },
+
+  {
+    id: 3,
+    name: "All Natural Italian-Style Chicken Meatballs",
+    imageUrl: <IconNgo />,
+    rating: 5,
+    originalPrice: 2.8,
+    discountedPrice: 2.51,
+    weight: "500 gram",
+    brand: "Hodo Foods",
+  },
+  {
+    id: 4,
+    name: "All Natural Italian-Style Chicken Meatballs",
+    imageUrl: <IconNgo />,
+    rating: 5,
+    originalPrice: 2.8,
+    discountedPrice: 2.51,
+    weight: "500 gram",
+    brand: "Hodo Foods",
+  },
+  {
+    id: 5,
+    name: "All Natural Italian-Style Chicken Meatballs",
+    imageUrl: <IconNgo />,
+    rating: 5,
+    originalPrice: 2.8,
+    discountedPrice: 2.51,
+    weight: "500 gram",
+    brand: "Hodo Foods",
+  },
+  
 ];
 export default productData;
