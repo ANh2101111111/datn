b80b87954f6222519f1a6dab979d5c0f526ab15f
lethihongnhan2004 <nl422864@gmail.com>
2025-02-15 00:18:53 +0700

    featute: created lables

--- src/layout/PopularProducts/index.tsx
+++ src/layout/PopularProducts/index.tsx
@@ -1,25 +1,36 @@
 import React from "react";
 import BoxListProduct from "./components/BoxListProduct";
 import productData from "./components/data";
+import BoxListLable from "../Lables/BoxListLable";
 
 const PopularProducts = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-[24px] mt-4 mb-4">
-      {productData.map((product) => (
-        <div key={product.id} className="flex justify-center">
-          <BoxListProduct
-            image={product.imageUrl}
-            name={product.name}
-            brand={product.brand}
-            rating={product.rating}
-            weight={product.weight}
-            originalPrice={product.originalPrice}
-            discountedPrice={product.discountedPrice}
-            type={product.type}
-            textType={product.textType}
-          />
-        </div>
-      ))}
+    <div className="container mx-auto px-4">
+      <div className="flex flex-row items-center justify-between w-full ">
+        <h2 className="text-heading-3 text-text-heading font-quicksand font-bold">
+          Popular Products
+        </h2>
+        <BoxListLable />
+      </div>
+
+      {/* Hiển thị danh sách sản phẩm */}
+      <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-6">
+        {productData.map((product) => (
+          <div key={product.id} className="flex justify-center">
+            <BoxListProduct
+              image={product.imageUrl}
+              name={product.name}
+              brand={product.brand}
+              rating={product.rating}
+              weight={product.weight}
+              originalPrice={product.originalPrice}
+              discountedPrice={product.discountedPrice}
+              type={product.type}
+              textType={product.textType}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
