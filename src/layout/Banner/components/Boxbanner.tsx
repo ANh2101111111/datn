4af0f4707bd24e6226185ddb7a46c995d612f180
lethihongnhan2004 <nl422864@gmail.com>
2025-02-21 14:37:52 +0700

    update: css banner

--- src/layout/Banner/components/Boxbanner.tsx
+++ src/layout/Banner/components/Boxbanner.tsx
@@ -16,7 +16,7 @@ const BoxBanner: React.FC<IBannerProps> = ({
   icon,
 }) => {
   return (
-    <div className="bg-blue-100 p-6 mt-28 rounded-xl flex flex-col md:flex-row items-center mb-4">
+    <div className="bg-blue-100 p-6 mt-4 rounded-xl flex flex-col md:flex-row items-center mb-4">
       <div className="flex-1">
         <h2 className=" lg:text-heading-1 md:text-heading-3 sm:text-heading-5  font-bold mb-5 font-quicksand text-text-heading">
           {title}
