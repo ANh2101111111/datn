24464ec52c706587fbbe701d863fb8433d1492bc
lethihongnhan2004 <nl422864@gmail.com>
2025-03-02 00:01:37 +0700

    update : add icon

---
+++ src/layout/assets/icons/IconDashBoard.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+const IconDashBoard: React.FC<React.SVGProps<SVGElement>> = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="25"
+    fill="none"
+    viewBox="0 0 24 25"
+  >
+    <path
+      fill="#B6B6B6"
+      d="M18 6.109a6 6 0 1 0-12 0H0v15a3 3 0 0 0 3 3h18a3 3 0 0 0 3-3v-15zm-6-4a4 4 0 0 1 4 4H8a4 4 0 0 1 4-4m10 19a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-13h4v2h2v-2h8v2h2v-2h4z"
+    ></path>
+  </svg>
+);
+export default IconDashBoard;
