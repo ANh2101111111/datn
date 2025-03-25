6dc905193492015fe95e1e5fb603dc85e0663243
ANh2101111111 <vananh21242004@gmail.com>
2025-03-26 00:13:06 +0700

    userprofile

--- src/main/java/datn/example/datn/web/rest/user/UserProfileController.java
+++ src/main/java/datn/example/datn/web/rest/user/UserProfileController.java
@@ -2,9 +2,13 @@ package datn.example.datn.web.rest.user;
 
 import datn.example.datn.dto.request.UserProfileRequest;
 import datn.example.datn.dto.response.UserProfileResponse;
+import datn.example.datn.entity.User;
+import datn.example.datn.repository.UserRepository;
 import datn.example.datn.service.UserProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController("userUserProfileController")
@@ -12,10 +16,14 @@ import org.springframework.web.bind.annotation.*;
 public class UserProfileController {
     @Autowired
     private UserProfileService userProfileService;
+    @Autowired
+    private UserRepository userRepository;
 
-    @GetMapping("/{userId}")
-    public ResponseEntity<UserProfileResponse> getUserProfile(@PathVariable Long userId) {
-        return ResponseEntity.ok(userProfileService.getUserProfile(userId));
+    @GetMapping
+    public ResponseEntity<UserProfileResponse> getUserProfile(@AuthenticationPrincipal UserDetails userDetails) {
+        User user = userRepository.findByUsername(userDetails.getUsername())
+                .orElseThrow(() -> new RuntimeException("User không tồn tại"));
+        return ResponseEntity.ok(userProfileService.getUserProfile(user.getUserId()));
     }
 
     @PostMapping("/{userId}")
